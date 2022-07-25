e63748f58b949e11ef2f0ae8231f28f070222cf8
mowemcfc <jcartermcfc@gmail.com>
2022-07-25 13:11:38 +1000

    Add channel DDB tables

--- provision/lib/discRSSStack.ts
+++ provision/lib/discRSSStack.ts
@@ -2,13 +2,24 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as cdk from 'aws-cdk-lib'
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 
 export class DiscRssStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'DiscRsScdkQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
+    const feedTable = new dynamodb.Table(this, 'FeedTable', {
+      partitionKey: {
+        name: 'feedID',
+        type: dynamodb.AttributeType.NUMBER
+      }
+    })
+
+    const channelTable = new dynamodb.Table(this, 'channelTable', {
+      partitionKey: {
+        name: 'channelID',
+        type: dynamodb.AttributeType.NUMBER
+      }
+    })
   }
 }
