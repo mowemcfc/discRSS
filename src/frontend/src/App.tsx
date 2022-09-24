907a907b996cc6f14c70bccb2bafc3bcc042cc0f
mowemcfc <jcartermcfc@gmail.com>
2022-09-24 16:48:54 +1000

    tidy: remove unused imports

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -3,17 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 import { LoginPage } from './components/login';
-import { LogoutButton } from './components/logout';
 import UserProfile from './components/profile';
-import { Auth0ContextInterface, useAuth0, withAuth0 } from "@auth0/auth0-react"
 
 import {
   Routes,
   Route,
-  redirect,
-  createBrowserRouter,
-  RouterProvider,
-  LoaderFunction,
   BrowserRouter
 } from 'react-router-dom';
 import { ProtectedRoute } from './components/protected';
--- src/frontend/src/components/profile.tsx
+++ src/frontend/src/components/profile.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { LogoutButton } from "./logout";
 
 const UserProfile = () => {
   const { user } = useAuth0();
@@ -9,6 +10,7 @@ const UserProfile = () => {
       <img src={user?.picture} alt={user?.name} />
       <h2>{user?.name}</h2>
       <p>{user?.email}</p>
+      <LogoutButton />
     </div>
   );
 };
